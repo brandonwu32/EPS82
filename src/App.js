309210baa9410157536aed0540e6f9d3476a68ef
brandonwu32 <brandonwu32@berkeley.edu>
2023-10-11 12:13:18 -0700

    added: background

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import './App.css';
+import Background from './static/ocean_background.svg';
 
 function App() {
-  return (
-    <div className="App">
-      Each metazoa will be a button that will toggle something like a menu that appears from the side and gives a description
+  /* Each metazoa will be a button that will toggle something like a menu that appears from the side and gives a description
 
       Clicking the button will toggle the menu for THAT metazoa and then the menu appears. The function used to activate the menu
       should also take in a prop that holds an exit function that can toggle the menu off.
 
       Each menu components should use the same css file to make life easier. We can also have all the menu come from the same js file
-      just that the component passes in props for things like animals in the family, features etc..
+      just that the component passes in props for things like animals in the family, features etc.. */
+  return (
+    <div className="App">
+      <img className = "background" src = {Background} alt = "none"></img>
     </div>
   );
 }
