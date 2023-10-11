cfdcf1a4d034c9168b3abac6d2ab2fb1b2177ef3
brandonwu32 <brandonwu32@berkeley.edu>
2023-10-11 11:53:19 -0700

    added: description files

---
+++ src/descriptions/description.js
@@ -0,0 +1,9 @@
+import "./description.css"
+
+function descriptions(props) {
+    return (
+        <div>What kinds of information should I display, what are the different paramters of props.</div>
+    )
+}
+
+export default descriptions;
